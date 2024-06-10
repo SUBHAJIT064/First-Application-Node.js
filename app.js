6ad3f8330477770c9e03d620ea9f5c00babb8f98
SUBHAJIT064 <102734135+SUBHAJIT064@users.noreply.github.com>
2024-06-10 12:35:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+	response.write('Hello from different World\n');
+	response.end();
+}
+
+const server = http.createServer(requestListener);
+
+const confirmationCallback = () => {
+	console.log('Server is running...');
+}
+
+server.listen(9000, confirmationCallback);
